fd0c29a17fd7a0b9ad8c5ff52ce8d1e6a60799d9
Brad Ringel <brad.ringel@gmail.com>
2018-05-28 12:30:39 -0400

    Use env for firebase keys to switch between dev and prod

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyDzBwWcz0GBhivtQ-aTMaDtl4i7OeHLMpI',
-  authDomain: 'vgbc-tracker.firebaseapp.com',
-  databaseURL: 'https://vgbc-tracker.firebaseio.com',
-  projectId: 'vgbc-tracker',
-  storageBucket: 'vgbc-tracker.appspot.com',
-  messagingSenderId: '221426422910'
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
 };
 
 const firebaseApp = firebase.initializeApp(config);
