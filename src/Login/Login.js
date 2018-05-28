e193ff7221d9196a3158d638b1a68a28e8c3f54f
Brad Ringel <brad.ringel@gmail.com>
2018-05-28 10:54:09 -0400

    Add styling and layout and sass support

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
+import Button from '../components/Button';
 import './Login.scss';
 
 class Login extends Component<{}> {
+  handleLoginClick = () => {};
+
   render() {
-    return <div className="login" />;
+    return (
+      <div className="login">
+        <Button className="login-button">Login in with Facebook</Button>
+      </div>
+    );
   }
 }
 
---
+++ src/components/Button.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './Button.scss';
+
+type Props = {
+  children: any,
+  buttonStyle: 'blue' | 'green' | 'red' | 'orange' | 'purple',
+  className: string
+};
+
+const button = (props: Props) => (
+  <button className={`btn ${props.buttonStyle}-button ${props.className}`}>{props.children}</button>
+);
+
+button.defaultProps = {
+  buttonStyle: 'blue',
+  className: ''
+};
+
+export default button;
