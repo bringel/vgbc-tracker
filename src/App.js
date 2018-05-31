9d31a3e797687acddd07e81073ed192c53231e57
Brad Ringel <brad.ringel@gmail.com>
2018-05-30 21:46:09 -0400

    Add dashboard page component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import firebase from './services/firebase';
 
 import Login from './Login/Login';
+import Dashboard from './Dashboard/Dashboard';
 
 import './App.scss';
 
@@ -24,7 +25,7 @@ class App extends Component<{}, State> {
     });
   }
   render() {
-    return <div className="app">{this.state.loggedIn ? null : <Login />}</div>;
+    return <div className="app">{this.state.loggedIn ? <Dashboard /> : <Login />}</div>;
   }
 }
 
---
+++ src/Dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import './Dashboard.scss';
+
+class Dashboard extends Component<{}> {
+  render() {
+    return <div className="dashboard" />;
+  }
+}
+
+export default Dashboard;
