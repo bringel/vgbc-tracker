0828e216742d4947171bd274e45e3988a089ff03
Brad Ringel <brad.ringel@gmail.com>
2018-07-03 17:12:56 -0400

    Add firebase deployment stuff

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,16 +3,23 @@ import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
-const config = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || '',
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || '',
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL || '',
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || '',
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || '',
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || ''
-};
+if (process.env.NODE_ENV === 'development') {
+  const config = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY || '',
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || '',
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL || '',
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || '',
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || '',
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || ''
+  };
 
-firebase.initializeApp(config);
+  firebase.initializeApp(config);
+} else if (process.env.NODE_ENV === 'production') {
+  fetch('/__/firebase/init.json').then((response) => {
+    // $FlowFixMe
+    firebase.initializeApp(response.json());
+  });
+}
 
 export const loginProvider = new firebase.auth.FacebookAuthProvider();
 export const database = firebase.firestore();
