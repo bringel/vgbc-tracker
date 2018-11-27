d152be3510393d761cb0030039c01d110723f328
Brad Ringel <brad.ringel@gmail.com>
2018-11-26 20:10:41 -0500

    Working on game history list

--- src/GamePreview/GamePreview.js
+++ src/GamePreview/GamePreview.js
@@ -4,6 +4,8 @@ import format from 'date-fns/format';
 
 import type { GameOfTheMonthGame } from '../types/Game';
 
+import './GamePreview.scss';
+
 type Props = {
   game: GameOfTheMonthGame
 };
@@ -12,7 +14,12 @@ class GamePreview extends React.Component<Props> {
   render() {
     const { game } = this.props;
     const activeDate = new Date(game.activeYear, game.activeMonth - 1);
-    return <div>{format(activeDate, 'MMMM')}</div>;
+    return (
+      <div className="game-preview">
+        {format(activeDate, 'MMMM')}
+        <div className="preview-info" />
+      </div>
+    );
   }
 }
 
