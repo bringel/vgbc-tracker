df1081c63c16a6faef620d27ec79577978443e36
Brad Ringel <brad.ringel@gmail.com>
2019-01-08 22:25:52 -0500

    change function to update to any role, require firebase token

--- functions/index.js
+++ functions/index.js
@@ -20,21 +20,36 @@ exports.handleUserSignUp = functions.auth.user().onCreate((user) => {
     );
 });
 
-exports.makeUserAdmin = functions.https.onRequest((request, response) => {
-  const { userID } = request.body;
+exports.updateRole = functions.https.onRequest((request, response) => {
+  const { userID, role } = request.body;
 
   const firestore = admin.firestore();
+  const auth = admin.auth();
 
-  return admin
-    .auth()
-    .setCustomUserClaims(userID, { role: 'admin' })
-    .then(() => {
-      return firestore
-        .collection('users')
-        .doc(userID)
-        .set({ role: 'admin' }, { merge: true });
-    })
-    .then(() => {
-      return response.send(200);
-    });
+  const authorization = request.headers.authorization;
+  const tokenMatcher = /Bearer (.*)/;
+
+  if (tokenMatcher.test(authorization)) {
+    const [_, token] = tokenMatcher.exec(authorization);
+
+    return auth
+      .verifyIdToken(token)
+      .then(() => {
+        return auth.setCustomUserClaims(userID, { role });
+      })
+      .then(() => {
+        return firestore
+          .collection('users')
+          .doc(userID)
+          .set({ role }, { merge: true });
+      })
+      .then(() => {
+        return response.sendStatus(200);
+      })
+      .catch(() => {
+        response.sendStatus(403);
+      });
+  } else {
+    return response.sendStatus(403);
+  }
 });
