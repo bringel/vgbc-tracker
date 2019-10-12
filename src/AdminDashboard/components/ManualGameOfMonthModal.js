f8773898988379a60afe407d60d2bb2d57727f14
Brad Ringel <brad.ringel@gmail.com>
2019-10-12 09:32:50 -0400

    Basic UI for manual game selection

--- src/AdminDashboard/components/ManualGameOfMonthModal.js
+++ src/AdminDashboard/components/ManualGameOfMonthModal.js
@@ -1,6 +1,10 @@
 //@flow
-import React from 'react';
-import { Modal, ModalHeader } from 'reactstrap';
+import addMonths from 'date-fns/add_months';
+import format from 'date-fns/format';
+import React, { useState } from 'react';
+import { Button, Form, FormGroup, Input, Label, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+
+import GameSearch from './GameSearch';
 
 type Props = {
   isOpen: boolean,
@@ -9,9 +13,54 @@ type Props = {
 
 const ManualGameOfMonthModal = (props: Props) => {
   const { isOpen, toggle } = props;
+  const today = new Date();
+  const nextMonth = addMonths(today, 1);
+  const [month, setMonth] = useState(today.getMonth());
+  const [game, setGame] = useState(null);
+
+  const handleRadioButton = e => {
+    setMonth(Number(e.target.value));
+  };
+
+  const handleSave = () => {};
+
   return (
-    <Modal isOpen={isOpen} toggle={toggle}>
+    <Modal isOpen={isOpen} toggle={toggle} size="xl" backdrop="static" scrollable>
       <ModalHeader toggle={toggle}>Set Game of the Month</ModalHeader>
+      <ModalBody>
+        <Form>
+          <FormGroup check inline>
+            <Label check>
+              <Input
+                type="radio"
+                name="month"
+                value={today.getMonth()}
+                checked={month === today.getMonth()}
+                onChange={handleRadioButton}
+              />
+              {format(today, 'MMMM')}
+            </Label>
+          </FormGroup>
+          <FormGroup check inline>
+            <Label check>
+              <Input
+                type="radio"
+                name="month"
+                value={nextMonth.getMonth()}
+                checked={month === nextMonth.getMonth()}
+                onChange={handleRadioButton}
+              />
+              {format(nextMonth, 'MMMM')}
+            </Label>
+          </FormGroup>
+        </Form>
+        <GameSearch onSelectionChanged={game => setGame(game)} />
+      </ModalBody>
+      <ModalFooter>
+        <Button disabled={!game} color="primary" onClick={handleSave}>
+          Save
+        </Button>
+      </ModalFooter>
     </Modal>
   );
 };
