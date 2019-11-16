0aeba68314cee9c43b68e3fb45a957ba70280bec
Brad Ringel <brad.ringel@gmail.com>
2019-11-16 11:31:37 -0500

    switch manual game selection modal to use funtion instead of adding document

--- src/AdminDashboard/components/ManualGameOfMonthModal.js
+++ src/AdminDashboard/components/ManualGameOfMonthModal.js
@@ -1,4 +1,5 @@
 //@flow
+import axios from 'axios';
 import { addMonths } from 'date-fns';
 import React, { useCallback, useState } from 'react';
 import { Button, ButtonGroup, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
@@ -25,24 +26,33 @@ const ManualGameOfMonthModal = (props: Props) => {
 
     const activeMonth = activeDate.getMonth() + 1;
     const activeYear = activeDate.getFullYear();
-    const games = gamesCollection();
 
-    const gameDoc = responseToGameOfTheMonthDocument(game, activeMonth, activeYear, true);
-
-    games
-      .add(gameDoc)
-      .then(ref => {
-        return games
-          .where('current', '==', true)
-          .get()
-          .then(querySnapshot => {
-            const ids = querySnapshot.docs.map(d => d.id).filter(id => id !== ref.id);
-            ids.forEach(id => {
-              games.doc(id).update({ current: false });
-            });
-          });
+    axios
+      .post('/selectGameOfTheMonth', {
+        gameID: game.id,
+        month: activeMonth,
+        year: activeYear
       })
       .then(() => toggle());
+
+    // const games = gamesCollection();
+
+    // const gameDoc = responseToGameOfTheMonthDocument(game, activeMonth, activeYear, true);
+
+    // games
+    //   .add(gameDoc)
+    //   .then(ref => {
+    //     return games
+    //       .where('current', '==', true)
+    //       .get()
+    //       .then(querySnapshot => {
+    //         const ids = querySnapshot.docs.map(d => d.id).filter(id => id !== ref.id);
+    //         ids.forEach(id => {
+    //           games.doc(id).update({ current: false });
+    //         });
+    //       });
+    //   })
+    //   .then(() => toggle());
   }, [game, month, toggle]);
 
   return (
